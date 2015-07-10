4119e9540bd436fee8ade6c7f6b60c69b585db21
Keith Bartholomew <keith.bartholomew@rackspace.com>
2015-07-10 07:21:13 -0500

    just make it a module

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-var fs = require('fs');
-var path = require('path');
-var stardpParser = require('./parsers/stardp');
-
-var STARDP_FILE = './data/STARDP.txt';
-
-console.log(stardpParser(STARDP_FILE));
+module.exports = {
+    stardp: require('./parsers/stardp')
+};
